9642085a7d89c7c0cb95ca938e11cafc9dca46b5
Angela <angelabecerraforero@gmail.com>
2022-02-16 11:05:52 -0500

    "Cambios en el HTML y JS HU No. 1"

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
-import App from './components/App.js';
+/* import App from './components/App.js'; */
 
-document.getElementById('root').appendChild(App());
+function mostrar(){
+    document.getElementById('pag1').style.display = 'none'; 
+    document.getElementById('pag2').style.display = 'block';
+    boton.addEventListener("click", mostrar());
+    console.log("Click");
+    
+}
+/* function paginaPrincipal(){
+    document.getElementById('pag1').style.display = ("block");
+    document.getElementById('pag2').style.display = ("none").value;
+    boton.addEventListener("click", paginaPrincipal); 
+} */
+
+    
+/* document.getElementById('root').appendChild(App()); */
