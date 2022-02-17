607aef7a4335e88b73c3537e66e67f4009581829
Angela <97766033+Angela2201@users.noreply.github.com>
2022-02-17 11:43:30 -0500

    Historia Usuario 1 (#1)
    * "Primer ejercicio de prueba HTML"
    
    * "Cambios en el HTML y JS HU No. 1"
    
    * Funcionalidad botón "jugar" y cambios CSS

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
-import App from './components/App.js';
+/* import App from './components/App.js';  */
+document.getElementById('pag2').style.display = 'none';
+const botonJugar = document.getElementById('botonInicial')
+botonJugar.addEventListener('click', () => {
+    console.log("Hice click")
+    document.getElementById('pag1').style.display = 'none';
+    document.getElementById('pag2').style.display = 'block';
+})
 
-document.getElementById('root').appendChild(App());
+/* /* function mostrar(){
+    document.getElementById('pag1').style.display = 'none'; 
+    document.getElementById('pag2').style.display = 'block';
+    boton.addEventListener("click", mostrar());
+    console.log("Click");
+     */
+/* } */ 
+/* function paginaPrincipal(){
+    document.getElementById('pag1').style.display = ("block");
+    document.getElementById('pag2').style.display = ("none").value;
+    boton.addEventListener("click", paginaPrincipal); 
+} */
+
+    
+/* document.getElementById('root').appendChild(App()); */
