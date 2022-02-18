e7e9f44d98c8c657091d37ebc64710e6ca652ec3
marisoldazaa <marisol.dazaa@gmail.com>
2022-02-18 17:34:07 -0500

    Se realizaron dos test sobre el formato HTML y sobre funcionalidad

--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -1,8 +1,24 @@
-import App from './App.js';
+import App from './App';
+/* import main from 'main.js'; Pensé que realizar esto es importante*/ 
+
 
 describe('App', () => {
   it('should render without crashing', () => {
     const el = App();
     expect(el instanceof HTMLElement).toBe(true);
+  });
+}); 
+
+/*Intento uno de crear un test para que una función exista */
+
+describe('have button', () => {
+  it('should be a button', () => {
+    expect (typeof HTMLElement) .toBe('function');
+  });
+}); 
+
+describe('document.getElementById', () => {
+  it('should be a function', () => {
+    expect ( typeof document.getElementById). toBe ('function')
   });
 });
--- src/main.js
+++ src/main.js
@@ -2,23 +2,8 @@
 document.getElementById('pag2').style.display = 'none';
 const botonJugar = document.getElementById('botonInicial')
 botonJugar.addEventListener('click', () => {
-    console.log("Hice click")
+    /*console.log("Hice click")*/
     document.getElementById('pag1').style.display = 'none';
     document.getElementById('pag2').style.display = 'block';
 })
 
-/* /* function mostrar(){
-    document.getElementById('pag1').style.display = 'none'; 
-    document.getElementById('pag2').style.display = 'block';
-    boton.addEventListener("click", mostrar());
-    console.log("Click");
-     */
-/* } */ 
-/* function paginaPrincipal(){
-    document.getElementById('pag1').style.display = ("block");
-    document.getElementById('pag2').style.display = ("none").value;
-    boton.addEventListener("click", paginaPrincipal); 
-} */
-
-    
-/* document.getElementById('root').appendChild(App()); */
