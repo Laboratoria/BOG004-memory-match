6a801db6d32cff221f2683d61bf2f70a4b308c3d
marisoldazaa <marisol.dazaa@gmail.com>
2022-03-01 10:10:41 -0500

    Inicio CSS, barajas o desorganizar las cartas

--- src/components/App.js
+++ src/components/App.js
@@ -8,41 +8,6 @@ export const App = (itemCartas) => {
   return traerData 
 }
   
-  
 
 
-  /* function myFunction() {
-      const node = document.getElementById("data");
-      const clone = node.cloneNode(true);
-      document.getElementById("contenedorTodas").appendChild(clone);
-  }
-} */
-  
-  /* function main() {
-    createPokemonsCards()
-  
-  }
-  
-  main()  */
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-/* let cartas = [] 
-  for(let i = 0; i < 2; i++) {
-    for(let a = 0; a < 9; a++) {
-      console.log("cartas", cartas)
-    }
-  }  */
 
--- src/main.js
+++ src/main.js
@@ -2,15 +2,18 @@
  import pokemon from '../data/pokemon/pokemon.js';
 console.log(pokemon);
 
-/* Este es el proceso donde la data se relaciona */
+/* Duplicar las cartas */
 let dataPokemon = pokemon.items
 dataPokemon = dataPokemon.concat(dataPokemon)
+
+ /*Creación de la página 2 y botón que mueve de una página a otra*/ 
 document.getElementById('pag2').style.display = 'none';
 const botonJugar = document.getElementById('botonInicial')
 botonJugar.addEventListener('click', () => {
     const contenedor = document.createElement('div');
+    contenedor.className = 'frontales';
     dataPokemon.forEach(element => {
-        contenedor.appendChild(App(element));
+        contenedor.appendChild(App(element)); /*Trae los elementos de data pokémon y sus caracteristicas*/
     });
     console.log(contenedor)
     /* Aqui se llama el id del 2ndo div de la pagina 2 para que se muestren las imagenes */
@@ -19,6 +22,7 @@ botonJugar.addEventListener('click', () => {
     document.getElementById('pag2').style.display = 'block';
 })
 
+/*Se crean las imagenes de la cara posterior y se multiplican*/
 function createCards(){
     const cardsContainerElement = document.getElementById("contenedorTodas");
     const cardBaseContainerHTML = '<div class="column card-container"><img class="card" data-pokemon-name"" src="https://cdn.pixabay.com/photo/2016/07/23/13/18/pokemon-1536849__480.png"></div>';
@@ -28,11 +32,25 @@ function createCards(){
       cardsContainerElement.innerHTML += cardBaseContainerHTML 
     }
 }
-
-
-
 function main() {
   createCards()
 }
 
 main()
+
+//Proceso de ver las cartas aleatoriamente
+const mezclar = () => { 
+  const tarjetas = dataPokemon;
+  tarjetas.sort ( ( ) => Math.random( ) - 0.5)
+};
+mezclar();
+
+// 
+
+
+ 
+
+
+
+
+
