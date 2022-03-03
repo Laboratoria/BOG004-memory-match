6faa581764aa2014e449385ddb402aa294383799
Angela <angelabecerraforero@gmail.com>
2022-03-03 08:41:50 -0500

    Se realizan cambios para poder girar las cartas, teniendo en cuenta que las pokebolas y los pokemones se han colocado en un mismo div

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ botonJugar.addEventListener('click', () => {
     const contenedor = document.createElement('div');
     contenedor.className = 'frontales';
     dataPokemon.forEach(element => {
-        contenedor.appendChild(App(element)); /*Trae los elementos de data pokémon y sus caracteristicas*/
+        /* contenedor.appendChild(App(element)); */ /*Trae los elementos de data pokémon y sus caracteristicas*/
     });
     console.log(contenedor)
     /* Aqui se llama el id del 2ndo div de la pagina 2 para que se muestren las imagenes */
@@ -25,12 +25,26 @@ botonJugar.addEventListener('click', () => {
 /*Se crean las imagenes de la cara posterior y se multiplican*/
 function createCards(){
     const cardsContainerElement = document.getElementById("contenedorTodas");
-    const cardBaseContainerHTML = '<div class="column card-container"><img class="card" data-pokemon-name"" src="https://cdn.pixabay.com/photo/2016/07/23/13/18/pokemon-1536849__480.png"></div>';
-    const pokemonsLength = dataPokemon.length;
-  
-    for (let index = 0; index < pokemonsLength; index++){
-      cardsContainerElement.innerHTML += cardBaseContainerHTML 
+    
+    for (let i = 0; i < dataPokemon.length; i++) {
+      const cardBaseContainerHTML = document.createElement("div")
+      cardBaseContainerHTML.setAttribute("class", "column card-container");
+      const imagePokemon = `<img class="card" data-pokemon-name"" src="https://cdn.pixabay.com/photo/2016/07/23/13/18/pokemon-1536849__480.png" id="pokebolas"><img src="${dataPokemon[i].image}" id="data">`
+      cardBaseContainerHTML.innerHTML= imagePokemon;
+      cardBaseContainerHTML.addEventListener("click", () =>{
+        console.log("Hola")
+        document.getElementById('pokebolas').style.display = 'none';
+        document.getElementById('data').style.display = 'block';
+      })
+      cardsContainerElement.appendChild(cardBaseContainerHTML)  
     }
+    
+    /* const pokemonsLength = dataPokemon.length; */
+    
+  
+    /* for (let index = 0; index < pokemonsLength; index++){
+      cardsContainerElement.innerHTML += cardBaseContainerHTML 
+    } */
 }
 
 
@@ -48,7 +62,7 @@ mezclar();
 
 // 
 
-
+ 
 
  
 
